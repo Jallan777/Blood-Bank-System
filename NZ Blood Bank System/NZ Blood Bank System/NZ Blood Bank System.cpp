8dba9f4b0c94379e68df1611e9c99c85a40606e6
Jallan777 <jacobstuartallan@gmail.com>
2021-11-17 21:32:34 +1300

    Fixed Vector Subscript Error.
    Looped recipient login upon no usernames found. CSV searcher still ain't working right

--- NZ Blood Bank System/NZ Blood Bank System/NZ Blood Bank System.cpp
+++ NZ Blood Bank System/NZ Blood Bank System/NZ Blood Bank System.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "Global functions.h"
+//#include "Global functions.h"
 #include <vector>
 
 using namespace std;
@@ -12,7 +12,7 @@ using namespace std;
 
 string rNameTemp, rPAddTemp, rEAddTemp, rNumTemp, rUNameTemp, rPWordTemp, recipValidTemp;
 string dFNameTemp, dLNameTemp, dDOBTemp, dDOBDay, dDOBMonth, dDOBYear, dNatTemp, dEthnicTemp, dGenderTemp, dUndCondTemp, dBloodTemp, dNumTemp, dEAddTemp, dPAddTemp, dLastDonateTemp, dUnameTemp, dPwordTemp;
-string recipientFilePath = "Patient info.csv"; // TODO MAKE SURE THIS IS RECIPIENT 
+string recipientFilePath = "RecipientInfo.csv"; // TODO MAKE SURE THIS IS RECIPIENT 
 string donorFilePath = "DonorInfo.csv";
 fstream file1, file2;
 
@@ -80,12 +80,165 @@ struct recipientInfo {
 
 void recipientWriteFunc();
 void donorWriteFunc();
+void menuFunc();
+
 
 //Simple Function to write a line of variable length, with any character
 void lineFunc(int length, string type) {
     for (int i = 0; i < length; i++)
         cout << type;
 }
+
+// for string delimiter                                                      <---- copied from stack overflow https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
+vector<string> split(string s, string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    string token;
+    vector<string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != string::npos) {
+        token = s.substr(pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back(token);
+    }
+
+    res.push_back(s.substr(pos_start));
+    return res;
+}
+
+
+//
+bool InfoExists(std::string target_info, string file_to_open, int where_to_look) {
+
+    ifstream file;
+    std::string line;
+    bool isHeader = true;
+
+    file.open(file_to_open, std::ios::in);
+    while (std::getline(file, line)) {
+        // this is the CSV header so skip it      
+        if (isHeader) {
+            isHeader = false;
+            continue;
+        }
+        // split the CSV line into an array
+        vector<string> values = split(line, ",");
+        // the name is at index[1]            
+        string info = values[where_to_look]; // the enum is one less then the column number
+        // check info against the correct column
+        if (info == target_info) {
+            // if we find the info in the right place, clean up our resources
+            // and return early
+            file.close();
+            return true;
+        }
+    }
+    // otherwise we didn't find the user,
+    // clean up our resources and return
+    file.close();
+    return false;
+}
+
+
+
+
+
+
+//Extract Donor Information from CSV File into struct
+//donorInfo Get_donor_info(int data_row_num, string file_to_open) { //TODO put a line from the csv into a struct and returning that struct
+//
+//    ifstream file;
+//    std::string line;
+//    bool isHeader = true;
+//    int row_counter = 0;
+//
+//    donorInfo info_to_return = {};
+//
+//    file.open(file_to_open, std::ios::in);
+//    while (std::getline(file, line)) {
+//        // this is the CSV header so skip it      
+//        if (isHeader) {
+//            isHeader = false;
+//            continue;
+//        }
+//        row_counter++; // make sure we are only looking at the desired row
+//
+//        if (data_row_num == row_counter) {
+//
+//
+//            // split the CSV line into an array
+//            // assign the values in the vector to the correct parts of the struct
+//            vector<string> values = split(line, ",");
+//            info_to_return.firstName = values[0];
+//            info_to_return.lastName = values[1];
+//            info_to_return.dob = values[2];
+//            info_to_return.nationality = values[3];
+//            info_to_return.ethnicity = values[4];
+//            info_to_return.gender = values[5];
+//            info_to_return.underlyCond = values[6];
+//            info_to_return.bloodGroup = values[7];
+//            info_to_return.contactNum = values[8];
+//            info_to_return.emailAddr = values[9];
+//            info_to_return.physAddr = values[10];
+//            info_to_return.lastDonation = values[11];
+//            info_to_return.username = values[12];
+//            info_to_return.password = values[13];
+//
+//            //close the file
+//            file.close();
+//
+//            //once its full with the info return it
+//            return info_to_return;
+//        }
+//
+//    } file.close();
+//    return info_to_return;
+//
+//
+//}
+
+recipientInfo Get_recipient_info(int data_row_num, string file_to_open) {
+
+    ifstream file;
+    std::string line;
+    bool isHeader = true;
+    int row_counter = 0;
+
+    recipientInfo info_to_return = {};
+
+    file.open(file_to_open, std::ios::in);
+    while (std::getline(file, line)) {
+        // this is the CSV header so skip it      
+        if (isHeader) {
+            isHeader = false;
+            continue;
+        }
+        row_counter++; // make sure we are only looking at the desired row
+
+        if (data_row_num == row_counter) {
+
+
+            // split the CSV line into an array
+            // assign the values in the vector to the correct parts of the struct
+            vector<string> values = split(line, ",");
+            info_to_return.recipientName = values[0];
+            info_to_return.physAddr = values[1];
+            info_to_return.emailAddr = values[2];
+            info_to_return.contactNum = values[3];
+            info_to_return.username = values[4];
+            info_to_return.password = values[5];
+            info_to_return.validated = values[6];
+
+            file.close();
+            return info_to_return;
+        }
+    } file.close();
+    return info_to_return;
+}
+
+//get_recip_info() TODO
+
+// get admin info() TODO
+
 
 //Recipient Registration Function
 void recipientRegFunc() {           
@@ -122,6 +275,7 @@ R8:    cout << "Password\t\t: "; getline(cin, rPWordTemp); if (rPWordTemp == "" 
 
 
 
+//Recipient Login Fucntion
 void recipientLoginFunc() {           
     
     //TODO check that the password exist and are correct - exception handle if not
@@ -136,7 +290,7 @@ void recipientLoginFunc() {
 
     // ask for username
     string temp_user;
-    cin.ignore();
+    
     cout << endl << "\tUsername: "; getline(cin, temp_user); // TODO not prompting the user here
     
     // check username exists in the csv by calling InfoExists() with hardcoded index and file to open
@@ -179,16 +333,29 @@ void recipientLoginFunc() {
     }   
     else {
         //tell user "username" isnt registered! 
-        cout << endl << "\tThe username " <<  temp_user  << " is not registered as a recipient!";
+        string rLoginRetry;
+        cout << endl << "\tThe username " <<  temp_user  << " is not registered as a recipient!" << endl; 
+    rLoginRetryRestart:
+        cout << endl << "\tDid you want to try again? [Y]/[N]" << endl << "\t"; cin >> rLoginRetry;
+        cin.ignore();
+        if (rLoginRetry == "Y" || rLoginRetry == "y") {
+            recipientLoginFunc();
+        }
+        else if (rLoginRetry == "N" || rLoginRetry == "n") {
+            cout << endl << "Returning to Menu..." << endl;
+            menuFunc();
+        }
+        else {
+            cout << endl << "Please Input a Valid Option" << endl;
+            goto rLoginRetryRestart;
+        }
         
-        
-        //return out of recipientLoginFunc //TODO
 
     }
     
 }
 
-
+//Donor Registraion Function
 void donorInfo::donorRegFunc() {               //Styled to have inputs all start on the same column
     struct donorInfo donor;
 
@@ -258,6 +425,7 @@ R16:    cout << "Password\t\t\t: "; getline(cin, dPwordTemp); if (dPwordTemp == 
     
 }
 
+//Donor Login Function
 void donorLoginFunc() { //TODO use very similar logic to recipientLoginFunc
     cout << endl << endl;
     cout << "\tDonor Login" << endl;
@@ -268,6 +436,7 @@ void donorLoginFunc() { //TODO use very similar logic to recipientLoginFunc
     cout << endl << endl;
 }
 
+//Admin Login Function
 void adminLoginFunc() { //TODO use very similar logic to recipientLoginFunc
     cout << endl << endl;
     cout << "\tAdministrator Login" << endl;
@@ -278,21 +447,23 @@ void adminLoginFunc() { //TODO use very similar logic to recipientLoginFunc
     cout << endl << endl;
 }
 
+//Recipient Write to CSV Function
 void recipientWriteFunc() {
 
     
     file1.open(recipientFilePath, ios::app);
     struct recipientInfo recipient;
     file1 << recipient.recipientName << ",";
-    file1 << " " << recipient.physAddr << ",";
-    file1 << "| " << recipient.emailAddr << ",";
-    file1 << "| " << recipient.contactNum << ",";
-    file1 << " " << recipient.username << ",";
-    file1 << " " << recipient.password << ",";
-    file1 << " " << recipient.validated << "\n";
+    file1 << recipient.physAddr << ",";
+    file1 << recipient.emailAddr << ",";
+    file1 << "|" << recipient.contactNum << ",";
+    file1 << recipient.username << ",";
+    file1 << recipient.password << ",";
+    file1 << recipient.validated << "\n";
     file1.close();
 }
 
+//Donor Write to CSV Function
 void donorWriteFunc() {
     file2.open(donorFilePath, ios::app);
     struct donorInfo donor;
@@ -304,7 +475,7 @@ void donorWriteFunc() {
     file2 << donor.gender << ",";
     file2 << donor.underlyCond << ",";
     file2 << donor.bloodGroup << ",";
-    file2 << donor.contactNum << ",";
+    file2 << "|" << donor.contactNum << ",";
     file2 << donor.emailAddr << ",";
     file2 << donor.physAddr << ",";
     file2 << donor.lastDonation << ",";
@@ -313,6 +484,7 @@ void donorWriteFunc() {
     file2.close();
 }
 
+//Main Menu Function
 void menuFunc() {
     donorInfo donor;
     recipientInfo recipient;
@@ -361,19 +533,20 @@ MenuRestart:
         cout << "[3] Admin Login" << endl;
         cout << endl << endl << "[4] Return to Menu" << endl;
         cout << endl << "Please select a valid option: "; cin >> loginChoice;
+        cin.ignore();
         if (loginChoice == "1") {
             donorLoginFunc();       //Portal to attach Donor Login
-            system("pause");
+            
             goto MenuRestart;
         }
         else if (loginChoice == "2") {
             recipientLoginFunc();   //Portal to attach Recipient Login
-            system("pause");
+            
             goto MenuRestart;
         }
         else if (loginChoice == "3") {
             adminLoginFunc();       //Portal to attach Admin Login (assuming we have a pre-made Admin User)
-            system("pause");
+            
             goto MenuRestart;
         }
         else if (loginChoice == "4") {
@@ -395,6 +568,7 @@ RegisterRestart:
         cout << "[2] Register as a Recipient" << endl;
         cout << endl << endl << "[3] Return to Menu" << endl;
         cout << endl << "Please select a valid option: "; cin >> regChoice;
+        cin.ignore();
         if (regChoice == "1") {
             
             donor.donorRegFunc();
@@ -421,12 +595,12 @@ RegisterRestart:
         exit(1);
         
     }
-    else if (menuInput == "6") {
+  /*  else if (menuInput == "6") {
         donorFrontPage();
     }
     else if (menuInput == "7") {
         recipFrontPage();
-    }
+    }*/
     else {
         cout << endl << "Please select a valid option" << endl;
         goto MenuRestart;
@@ -437,113 +611,6 @@ RegisterRestart:
 
 
 
-
-
-// for string delimiter                                                      <---- copied from stack overflow https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
-vector<string> split(string s, string delimiter) {
-    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-    string token;
-    vector<string> res;
-
-    while ((pos_end = s.find(delimiter, pos_start)) != string::npos) {
-        token = s.substr(pos_start, pos_end - pos_start);
-        pos_start = pos_end + delim_len;
-        res.push_back(token);
-    }
-
-    res.push_back(s.substr(pos_start));
-    return res;
-}
-
-
-bool InfoExists(std::string target_info, string file_to_open, int where_to_look) {
-
-    ifstream file;
-    std::string line;
-    bool isHeader = true;
-
-    file.open(file_to_open, std::ios::in);
-    while (std::getline(file, line)) {
-        // this is the CSV header so skip it      
-        if (isHeader) {
-            isHeader = false;
-            continue;
-        }
-        // split the CSV line into an array
-        vector<string> values = split(line, ",");
-        // the name is at index[1]            
-        string info = values[where_to_look]; // the enum is one less then the column number
-        // check info against the correct column
-        if (info == target_info) {
-            // if we find the info in the right place, clean up our resources
-            // and return early
-            file.close();
-            return true;
-        }
-    }
-    // otherwise we didn't find the user,
-    // clean up our resources and return
-    file.close();
-    return false;
-}
-
-donorInfo Get_donor_info(int data_row_num, string file_to_open) { //TODO put a line from the csv into a struct and returning that struct
-
-    ifstream file;
-    std::string line;
-    bool isHeader = true;
-    int row_counter = 0;
-
-    donorInfo info_to_return = {};
-
-    file.open(file_to_open, std::ios::in);
-    while (std::getline(file, line)) {
-        // this is the CSV header so skip it      
-        if (isHeader) {
-            isHeader = false;
-            continue;
-        }
-        row_counter++; // make sure we are only looking at the desired row
-
-        if (data_row_num == row_counter) {
-
-
-            // split the CSV line into an array
-            // assign the values in the vector to the correct parts of the struct
-            vector<string> values = split(line, ",");
-            info_to_return.firstName = values[0];
-            info_to_return.lastName = values[1];
-            info_to_return.dob = values[2];
-            info_to_return.nationality = values[3];
-            info_to_return.ethnicity = values[4];
-            info_to_return.gender = values[5];
-            info_to_return.underlyCond = values[6];
-            info_to_return.bloodGroup = values[7];
-            info_to_return.contactNum = values[8];
-            info_to_return.emailAddr = values[9];
-            info_to_return.physAddr = values[10];
-            info_to_return.lastDonation = values[11];
-            info_to_return.username = values[12];
-            info_to_return.password = values[13];
-
-            //close the file
-            file.close();
-
-            //once its full with the info return it
-            return info_to_return;
-        }
-
-    } file.close();
-    return info_to_return;
-
-
-}
-
-
-
-//get_recip_info() TODO
-
-// get admin info() TODO
 
 
 
